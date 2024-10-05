58ef6c1f431d4ba225fc21d8d5c3a0bb5b1ee735
Fredrik Lundell <fredrik_lundell@icloud.com>
2024-10-05 13:27:57 +0200

    commit server

---
+++ src/test/java/com/freddan/mediaproject_server/MediaprojectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freddan.mediaproject_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaprojectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
